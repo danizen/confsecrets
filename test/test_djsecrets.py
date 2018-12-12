703da241f9987248a194646d40c76e60c37b1b23
Dan Davis <dan@danizen.net>
2018-12-11 22:15:59 -0500

    Increase test coverage by calling django commands.

---
+++ test/test_djsecrets.py
@@ -0,0 +1,33 @@
+import pytest
+from base64 import b64decode
+from unittest import TestCase
+
+try:
+    from django.core.management import call_command
+    have_django = True
+except ImportError:
+    have_django = False
+
+
+if not have_django:
+    pytest.skip('Django is not installed')
+
+
+def test_new_salt_raw(capsys):
+    call_command('newsalt')
+    captured = capsys.readouterr()
+    assert len(captured.err) == 0
+    new_salt = eval(captured.out)
+    assert isinstance(new_salt, bytes)
+    assert len(new_salt) == 8
+
+
+def test_new_salt_encoded(capsys):
+    call_command('newsalt', '--encode')
+    captured = capsys.readouterr()
+    assert len(captured.err) == 0
+    new_salt = b64decode(captured.out)
+    assert isinstance(new_salt, bytes)
+    assert len(new_salt) == 8
+
+
