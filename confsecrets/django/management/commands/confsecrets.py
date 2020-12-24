3cfeb4096296500182120cd755a6fdf838261a85
Dan Davis <dan@danizen.net>
2020-12-24 18:53:32 -0500

    Put more apps into the settings
    - This is partly to see what's wrong with the App.

--- confsecrets/django/management/commands/confsecrets.py
+++ confsecrets/django/management/commands/confsecrets.py
@@ -2,6 +2,7 @@ from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
+    help = 'The confsecrets CLI with Django configuration'
 
-    def handle(self):
+    def handle(self, *args, **options):
         print('Not yet implemented')
--- test/django/settings.py
+++ test/django/settings.py
@@ -4,6 +4,9 @@ from confsecrets import Secret
 SECRET_KEY = 'eq3e9rklb)q5k@!@d^^94l4ldut)uev3#axpj_3j62&$upocex'
 
 INSTALLED_APPS = [
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.auth',
     'confsecrets.django',
 ]
 
